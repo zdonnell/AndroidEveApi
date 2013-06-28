85b60d6dabacae2bbf6903dbc507ede8c69b0085
Zach Donnell <slick50zd1@gmail.com>
2013-06-27 23:40:44 -0700

    ApiTasks were not correctly updating callback state

--- src/main/java/com/zdonnell/androideveapi/link/ApiCachingTask.java
+++ src/main/java/com/zdonnell/androideveapi/link/ApiCachingTask.java
@@ -9,35 +9,34 @@ import com.zdonnell.androideveapi.core.ApiResponse;
  * Base class for Tasks that need to maintain a cache of the ApiResponse.
  * 
  * @author Zach
- *
+ * 
  * @param <ExecuteParameter>
  * @param <ProgressParameter>
  * @param <Response>
  */
-public abstract class ApiCachingTask<ExecuteParameter, ProgressParameter, Response extends ApiResponse> extends ApiTask<ExecuteParameter, ProgressParameter, Response>
-{
+public abstract class ApiCachingTask<ExecuteParameter, ProgressParameter, Response extends ApiResponse> extends ApiTask<ExecuteParameter, ProgressParameter, Response> {
 	/**
-	 * True if the {@link CacheDatabase} has an entry for the given Response hash and {@link #useCache} is true.
+	 * True if the {@link CacheDatabase} has an entry for the given Response
+	 * hash and {@link #useCache} is true.
 	 */
 	private boolean cacheExists = false;
-	
+
 	/**
-	 * True if the {@link CacheDatabase} has an entry for the given Response hash that
-	 * has not yet expired and {@link #useCache} is true.
+	 * True if the {@link CacheDatabase} has an entry for the given Response
+	 * hash that has not yet expired and {@link #useCache} is true.
 	 */
 	private boolean cacheValid = false;
-	
+
 	/**
 	 * Database storing hash codes and the time their cache data expires
 	 */
 	private CacheDatabase cacheDatabase;
-	
+
 	/**
 	 * Stored response built from cached data, if available
 	 */
 	protected Response cachedData;
-	
-	
+
 	public ApiCachingTask(ApiExceptionCallback<Response> callback, Context context, boolean useCache, ApiAuth<?> apiAuth, EveApiInteraction<Response> apiInteraction) {
 		super(callback, context, useCache, apiAuth, apiInteraction);
 		this.cacheDatabase = new CacheDatabase(context);
@@ -45,57 +44,59 @@ public abstract class ApiCachingTask<ExecuteParameter, ProgressParameter, Respon
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected Response doInBackground(ExecuteParameter... params) 
-	{
+	protected Response doInBackground(ExecuteParameter... params) {
 		int requestHash = requestTypeHash();
-		if (apiAuth != null) requestHash += apiAuth.hashCode();
-		
+		if(apiAuth != null)
+			requestHash += apiAuth.hashCode();
+
 		cacheValid = cacheDatabase.cacheValid(requestHash);
 		cacheExists = cacheDatabase.cacheExists(requestHash);
-		
-		if (cacheValid) {
+
+		if(cacheValid) {
 			return buildResponseFromDatabase();
-		} else {	
-			// The cache is out of date (invalid) but load it anyway while we contact the API server
-			if (cacheExists) {
+		} else {
+			// The cache is out of date (invalid) but load it anyway while we
+			// contact the API server
+			if(cacheExists) {
 				cachedData = buildResponseFromDatabase();
 				publishProgress();
-			} else callback.updateState(ApiExceptionCallback.STATE_CACHED_RESPONSE_NOT_FOUND);
-			
+			} else
+				callback.updateState(ApiExceptionCallback.STATE_CACHED_RESPONSE_NOT_FOUND);
+
 			Response response = super.doInBackground(params);
-			if (response != null) cacheDatabase.updateCache(requestHash, response.getCachedUntil());
-			
-	        return response;
+			if(response != null)
+				cacheDatabase.updateCache(requestHash, response.getCachedUntil());
+
+			return response;
 		}
 	}
-	
+
 	@Override
-	protected void onProgressUpdate(ProgressParameter... progress)
-	{		
+	protected void onProgressUpdate(ProgressParameter... progress) {
 		callback.updateState(ApiExceptionCallback.STATE_CACHED_RESPONSE_ACQUIRED_INVALID);
 		callback.onUpdate(cachedData);
 	}
-	
+
 	@Override
-	protected void onPostExecute(Response response) 
-	{
-		// We can arrive here one of two ways, if the cache was still valid, or if it was invalid
-		// and a server response was acquired, check which it is.
-		if (cacheValid) {
+	protected void onPostExecute(Response response) {
+		// We can arrive here one of two ways, if the cache was still valid, or
+		// if it was invalid and a server response was acquired, check which it
+		// is.
+		if(cacheValid) {
 			callback.updateState(ApiExceptionCallback.STATE_CACHED_RESPONSE_ACQUIRED_VALID);
 			callback.onUpdate(response);
 		} else {
 			super.onPostExecute(response);
 		}
-    }
-	
+	}
+
 	/**
 	 * Subclasses implement this to specify a unique hash for that request
 	 * 
 	 * @return
 	 */
 	protected abstract int requestTypeHash();
-	
+
 	/**
 	 * Subclasses implement this to provide a Response built from the database
 	 * 
--- src/main/java/com/zdonnell/androideveapi/link/ApiTask.java
+++ src/main/java/com/zdonnell/androideveapi/link/ApiTask.java
@@ -8,10 +8,11 @@ import com.zdonnell.androideveapi.core.ApiResponse;
 import com.zdonnell.androideveapi.exception.ApiException;
 
 /**
- * Base AsyncTask for acquiring API Data.  This class does not handle caching of data.
+ * Base AsyncTask for acquiring API Data. This class does not handle caching of
+ * data.
  * 
  * @author Zach
- *
+ * 
  * @param <ExecuteParameter>
  * @param <ProgressParameter>
  * @param <Response>
@@ -24,72 +25,73 @@ public abstract class ApiTask<ExecuteParameter, ProgressParameter, Response exte
 	 * Callback to provide the the acquired response to.
 	 */
 	protected final ApiExceptionCallback<Response> callback;
-	
+
 	/**
 	 * Code to run to generate the response, provided by subclass
 	 */
 	final private EveApiInteraction<Response> apiInteraction;
-	
+
 	/**
-	 * Flags whether an exception occurred when the {@link #apiInteraction} was run
+	 * Flags whether an exception occurred when the {@link #apiInteraction} was
+	 * run
 	 */
 	protected boolean apiExceptionOccured = false;
-	
+
 	/**
 	 * Stores the ApiException if one occurs in {@link #apiInteraction}
 	 */
 	protected ApiException exception;
-	
+
 	/**
 	 * Application context
 	 */
 	final protected Context context;
-	
+
 	/**
 	 * Reference to the apiAuthorization used
 	 */
 	protected final ApiAuth<?> apiAuth;
-	
+
 	public ApiTask(ApiExceptionCallback<Response> callback, Context context, boolean useCache, ApiAuth<?> apiAuth, EveApiInteraction<Response> apiInteraction) {
 		this.callback = callback;
 		this.apiInteraction = apiInteraction;
 		this.context = context;
 		this.apiAuth = apiAuth;
-		
-		callback.updateState(ApiExceptionCallback.STATE_CACHED_RESPONSE_ACQUIRED_INVALID);
+
+		if(!(this instanceof ApiCachingTask))
+			callback.updateState(ApiExceptionCallback.STATE_CACHED_RESPONSE_ACQUIRED_INVALID);
 	}
-	
+
 	@Override
-	protected Response doInBackground(ExecuteParameter... params) 
-	{	
+	protected Response doInBackground(ExecuteParameter... params) {
 		Response response = null;
-		
-        try { 
-        	response = apiInteraction.perform();	        	
-        } catch (ApiException e) {
+
+		try {
+			response = apiInteraction.perform();
+		} catch(ApiException e) {
 			apiExceptionOccured = true;
 			exception = e;
 		}
-        return response;
+		return response;
 	}
-	
+
 	@Override
 	protected void onPostExecute(Response response) {
-		if (apiExceptionOccured) {
+		if(apiExceptionOccured) {
 			callback.updateState(ApiExceptionCallback.STATE_SERVER_RESPONSE_FAILED);
 			callback.onError(response, exception);
-		} else  {
+		} else {
 			callback.updateState(ApiExceptionCallback.STATE_SERVER_RESPONSE_ACQUIRED);
 			callback.onUpdate(response);
 		}
-    }
-	
+	}
+
 	/**
 	 * Interface to be implemented for interaction with the eveapi data specific
 	 * to the current request.
 	 * 
 	 * @author Zach
-	 *
+	 * 
 	 * @param <Response>
 	 * @see {@link ApiTask#apiInteraction}
 	 */
